64594e043dc62ce8c8ccf8f5b153713229afd12d
Martin <martingauna2000@gmail.com>
2022-02-09 10:55:33 -0300

    fix style buttom

--- src/components/Products/index.js
+++ src/components/Products/index.js
@@ -5,26 +5,66 @@ export const ProductsList = () => {
   return (
     <>
       <h1 className="title">PRODUCTOS</h1>
-      <div className="productos"> 
-      <div className="producto">
-      <a href="#">
-        <div className="producto__img">
-          <img src={IMG} alt="" />
-        </div>
-        </a>
-        <div className="producto__footer">
-          <h1>Titulo</h1>
-          <p>Categoria</p>
-          <p className="price">$320</p>
-        </div>
-        <div className="buttom">
-          <button className="btn">
-            Añadir al carrito
-          </button>
-          <div>
-            <a href="#" className="btn">Vista</a>
+      <div className="productos">
+        <div className="producto">
+          <a href="#">
+            <div className="producto__img">
+              <img src={IMG} alt="" />
+            </div>
+          </a>
+          <div className="producto__footer">
+            <h1>Titulo</h1>
+            <p>Categoria</p>
+            <p className="price">$320</p>
+          </div>
+          <div className="buttom">
+            <button className="btn">Añadir al carrito</button>
+            <div>
+              <a href="#" className="btn">
+                Vista
+              </a>
+            </div>
           </div>
         </div>
+        <div className="producto">
+          <a href="#">
+            <div className="producto__img">
+              <img src={IMG} alt="" />
+            </div>
+          </a>
+          <div className="producto__footer">
+            <h1>Titulo</h1>
+            <p>Categoria</p>
+            <p className="price">$320</p>
+          </div>
+          <div className="buttom">
+            <button className="btn">Añadir al carrito</button>
+            <div>
+              <a href="#" className="btn">
+                Vista
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="producto">
+          <a href="#">
+            <div className="producto__img">
+              <img src={IMG} alt="" />
+            </div>
+          </a>
+          <div className="producto__footer">
+            <h1>Titulo</h1>
+            <p>Categoria</p>
+            <p className="price">$320</p>
+          </div>
+          <div className="buttom">
+            <button className="btn">Añadir al carrito</button>
+            <div>
+              <a href="#" className="btn">
+                Vista
+              </a>
+            </div>
+          </div>
         </div>
       </div>
     </>
